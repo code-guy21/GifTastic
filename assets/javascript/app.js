96ea6c3350650eef56c1c19c312db6460dc4f8ca
Alexis San Javier <alexis@alexiss-mbp.lan>
2020-05-09 23:33:02 -0400

    added additional styling to form and page sections

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,7 +50,7 @@ const addTopic = (value) => {
           class: " fas fa-times",
         })
         .css({
-          padding: "8px",
+          padding: "12px",
         })
     );
 
@@ -65,7 +65,7 @@ const addTopic = (value) => {
           class: " far fa-heart",
         })
         .css({
-          padding: "8px",
+          padding: "12px",
           color: "red",
         })
     );
