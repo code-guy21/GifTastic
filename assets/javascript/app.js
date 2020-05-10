93baf7ef17c0deb24dc1667f6ed7e96197916171
Alexis San Javier <alexis@alexiss-mbp.lan>
2020-05-09 22:44:11 -0400

    refactored code, added comments, and additional fonts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,45 +19,31 @@ const API_KEY = "GG3nuwnZU6fm89I1KysFNlx7x9sKXaZH";
 const base_url = "https://api.giphy.com/v1/gifs/";
 
 const populate = (data, method) => {
-  //loop through each GIF object
+  //loop through data
   data.forEach((data) => {
-    //call addGif function
+    //call method
     method(data);
   });
 };
 
-const addTopic = (topic) => {
-  let contain = $("<div>")
-    .attr({
-      id: topic,
-      class: "btn-group",
-    })
-    .css({
-      display: "flex",
-      "flex-direction": "row",
-      width: "150px",
-      border: "1px solid grey",
-      margin: "5px",
-      "border-radius": "5px",
-    });
+const addTopic = (value) => {
+  //create container for buttons
+  let contain = $("<div>").attr({
+    id: value,
+    class: "btn-group flex row",
+  });
 
-  let btn = $("<div>")
-    .text(topic)
-    .attr({
-      value: topic,
-      class: "topic",
-    })
-    .css({
-      width: "80%",
-      padding: "8px",
-      "font-family": "sans-serif",
-    });
+  //create topic button
+  let topic = $("<span>").text(value).attr({
+    value: value,
+    class: "topic",
+  });
 
+  //create remove button
   let remove = $("<span>")
     .attr({
       class: "remove",
     })
-    .css("width", "20%")
     .append(
       $("<i>")
         .attr({
@@ -68,11 +54,11 @@ const addTopic = (topic) => {
         })
     );
 
+  //create favorite button
   let fav = $("<span>")
     .attr({
       class: "fav",
     })
-    .css("width", "20%")
     .append(
       $("<i>")
         .attr({
@@ -84,15 +70,16 @@ const addTopic = (topic) => {
         })
     );
 
-  contain.append(fav).append(btn).append(remove);
+  //append buttons to container
+  contain.append(fav).append(topic).append(remove);
 
-  //add GIF to page
+  //add container to topics page
   $("#topics").append(contain);
 };
 
 const addGif = (gif) => {
-  //create GIF image element
-  let img = $("<button>")
+  //create GIF element
+  let btn = $("<button>")
     .attr({
       src: gif.images.fixed_height_still.url,
       id: gif.id,
@@ -103,27 +90,26 @@ const addGif = (gif) => {
     })
     .css({
       "background-image": "url(" + gif.images.fixed_height_still.url + ") ",
-      "background-size": "100% 100%",
       height: gif.images.fixed_height.height,
       width: gif.images.fixed_height.width,
-      position: "relative",
     });
 
-  let rating = $("<div>" + gif.rating + "</div>").attr("class", "rating");
+  //create rating element
+  let rating = $("<span>" + gif.rating + "</span>").attr("class", "rating");
 
-  img.append(rating);
+  //attach rating to GIF
+  btn.append(rating);
+
   //add GIF to page
-  $("#images").append(img);
+  $("#images").append(btn);
 };
 
-//handles requests to GIPHY API
 const requestGifs = (query) => {
   $.ajax({
     url: query,
     method: "GET",
   })
     .then((response) => {
-      console.log(response, query);
       if (response.data.length === 0) {
         $("#images").html("<div>no GIFs found</div>");
       } else {
@@ -137,15 +123,43 @@ const requestGifs = (query) => {
     });
 };
 
+const remove = (array, btn) => {
+  //find the index in array
+  let index = array.indexOf(btn.closest("div").attr("id"));
+
+  //check if index was found
+  if (index !== -1) {
+    //remove element from array
+    array.splice(index, 1);
+
+    //remove element from  page
+    btn.closest("div").remove();
+  }
+};
+
+const changeState = (gif, state) => {
+  //change background image
+  gif.css({
+    "background-image": "url(" + gif.attr(state) + ")",
+  });
+
+  //change state of GIF
+  gif.attr("state", state);
+};
+
 //Application starts on document load
 $(document).ready(() => {
-  //populate topics with buttons
+  //populate topics section
   populate(topics, addTopic);
 
+  //listener for topic buttons
   $(document).on("click", ".topic", function () {
+    //store topic
     let topic = $(this).text();
+
+    //check if topic is not currently selected
     if (!current || current !== topic) {
-      current = topic;
+      //hit different endpoint for 'trending' and 'search' topic
       let query =
         topic === "trending"
           ? base_url + "trending?api_key=" + API_KEY + "&limit=10"
@@ -156,63 +170,77 @@ $(document).ready(() => {
             topic +
             "&limit=10";
 
-      //clear previous GIFs
+      //clear previous GIFs on page
       $("#images").empty();
+
+      //make request to GIPHY API
       requestGifs(query);
+
+      //update currently selected topic
+      current = topic;
     }
   });
 
+  //listener for GIF images
   $(document).on("click", ".gif", function () {
+    //grab reference to GIF
     let gif = $(this);
-    if (gif.attr("state") === "still") {
-      gif.css({
-        "background-image": "url(" + gif.attr("animate") + ")",
-      });
 
-      gif.attr("state", "animate");
-    } else {
-      gif.css({
-        "background-image": "url(" + gif.attr("still") + ")",
-      });
-      gif.attr("state", "still");
-    }
+    //change state of GIF
+    gif.attr("state") === "still"
+      ? changeState(gif, "animate")
+      : changeState(gif, "still");
   });
 
+  //listener for remove button
   $(document).on("click", ".remove", function () {
-    let topic = $(this);
+    //grab reference to remove button
+    let btn = $(this);
 
-    if (topic.closest(".container").attr("id") === "topic-container") {
-      let topic_index = topics.indexOf($(this).closest("div").attr("id"));
-      if (topic_index !== -1) {
-        topics.splice(topic_index, 1);
-        $(this).closest("div").remove();
-      }
-    } else {
-      let fav_index = favorites.indexOf($(this).closest("div").attr("id"));
-      if (fav_index !== -1) {
-        favorites.splice(fav_index, 1);
-        $(this).closest("div").remove();
-      }
-    }
+    //remove button from favorites or topics section
+    btn.closest(".container").attr("id") === "topic-container"
+      ? remove(topics, btn)
+      : remove(favorites, btn);
   });
 
+  //listener for favorite button
   $(document).on("click", ".fav", function () {
-    let topic = $(this).closest("div");
+    //grab reference to parent div
+    let parent = $(this).closest("div");
 
-    if (!favorites.includes(topic.attr("id"))) {
-      let clone = topic.clone();
+    //check if topic is already part of favorites list
+    if (!favorites.includes(parent.attr("id"))) {
+      //make a clone of parent div
+      let clone = parent.clone();
+
+      //remove favorite button from clone
       clone.children(".fav").remove();
-      clone.appendTo("#favorites");
+
+      //add clone to favorites section
+      $("#favorites").append(clone);
+
+      //add topic to favorites list
       favorites.push(clone.attr("id"));
     }
   });
 
+  //listener for submit button
   $("#submit").click(() => {
+    //prevent default HTML behavior
     event.preventDefault();
+
+    //grab value from input field
     let topic = $("#add").val().toLowerCase().trim();
+
+    //check if topic has already been added or if input is empty
     if (!topics.includes(topic) && topic !== "") {
+      //add button to topic section on page
       addTopic(topic);
+
+      //add topic to array of topics
       topics.push(topic);
+
+      //clear input field
       $("#add").val("");
     }
   });
